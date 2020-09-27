29c9c1c9a5dca70c85390d974bd36f5954694531
erickngumbau <57805450+erickngumbau@users.noreply.github.com>
2020-09-27 04:15:30 +0300

    Erick Portfolio first commit

---
+++ frontend/views/site/about.php
@@ -0,0 +1,50 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="row">
+            <div class="col-lg-4">
+                <h2>About Us</h2>
+
+                <p>Whether it's a food truck or a fashion line, a coffee shop or a consulting firm, 
+                SoftWare Develop's goal is to help entrepreneurs build the business of their 
+                    dreams and to assist anyone working in a small business make smart decisions about 
+                    products, services and ideas. Our coding style is simple: We have a team of well 
+                    trained developers with adequate skills to come up with a smart final output.</p>
+
+                <p>
+                
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p> -->
+            </div>
+            <div class="col-lg-4">
+                <h2>Mission Statement</h2>
+
+                <p>To provide the ideas, inspiration and solutions needed to help entrepreneurs and 
+                    small business decision makers succeed.</p>
+
+                <p>
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p> -->
+            </div>
+            <div class="col-lg-4">
+                <h2>Our History</h2>
+
+                <p>SoftWare Develop was founded in 2010 as a resource for small business owners
+                     at all stages of their entrepreneurial journey. Our site is focused exclusively
+                      on giving small business advice, tutorials and insider insights. SoftWare Develop
+                       is owned by SoftWare Develop.com.</p>
+
+                <p>
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p> -->
+            </div>
+        </div>
+
+    <!-- <code><?= __FILE__ ?></code> -->
+</div>
---
+++ frontend/views/site/index.php
@@ -0,0 +1,53 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Software Develop</h1>
+
+        <p class="lead">Get your sotware application/project Developed.</p>
+
+        <p>
+            <!-- <a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Android</h2>
+
+                <p>You have an Android based project? You are at the Right site. Android is a mobile operating system based on a modified version of the Linux kernel and
+                     other open source software, designed primarily for touchscreen mobile devices such as 
+                     smartphones and tablets. Get your application developed as per your specifics, using either Java or Kotlin.</p>
+
+                <p>
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p> -->
+            </div>
+            <div class="col-lg-4">
+                <h2>Yii</h2>
+
+                <p>Yii is an open source, object-oriented, component-based MVC PHP web application framework. 
+                    Yii is pronounced as "Yee" or [ji:] and in Chinese it means "simple and evolutionary" and 
+                    it can be an acronym for "Yes It Is!"..</p>
+
+                <p>
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p> -->
+            </div>
+            <div class="col-lg-4">
+                <h2>Pure Java Systems</h2>
+
+                <p>You have a Java based system to be developed? Other than Android systems, 
+                    we do develop pure Java systems.</p>
+
+                <p>
+                    <!-- <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p> -->
+            </div>
+        </div>
+
+    </div>
+</div>
